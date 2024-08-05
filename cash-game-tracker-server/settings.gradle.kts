9ae0953897ff8b2e76dad28fec229bbaa8575638
Cowan Cumming <ctcumming@gmail.com>
2024-08-05 14:08:43 -0700

    Initial commit with frontend and backend

---
+++ cash-game-tracker-server/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "cash-game-tracker"
---
+++ cash-game-tracker-server/src/test/kotlin/com/cowan/cashgametracker/CashGameTrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cowan.cashgametracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CashGameTrackerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
