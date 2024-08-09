b4686f6475780d3c6dc7a282b23aa09d5dd2c898
Cowan Cumming <ctcumming@gmail.com>
2024-08-09 00:34:22 -0700

    Simplify CSS

--- cash-game-tracker-web/app/page.tsx
+++ cash-game-tracker-web/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function Button(props: { href: string, label: string }) {
   return (
-    <Link href={props.href} className="flex items-center gap-5 rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base self-center">
+    <Link href={props.href} className="flex items-center gap-5 rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base">
       <span>{props.label}</span>
     </Link>
   )
@@ -18,8 +18,8 @@ function DummyButton(props: { href: string, label: string }) {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen">
-      <div className="flex flex-col gap-2 self-center flex-grow">
+    <main className="flex min-h-screen justify-center">
+      <div className="flex flex-col gap-2 justify-center">
         <Button href="/game" label="Host Game" />
         <Button href="/join-game" label="Join Game" />
       </div>
