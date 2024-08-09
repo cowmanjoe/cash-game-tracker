4cf8074f032f9a5545246995f413ef6228b5067b
Cowan Cumming <ctcumming@gmail.com>
2024-08-08 23:52:38 -0700

    Convert main page to use tailwind

--- cash-game-tracker-web/app/page.tsx
+++ cash-game-tracker-web/app/page.tsx
@@ -1,25 +1,28 @@
-import styles from "./page.module.css";
-import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
-import Link from "@mui/material/Link";
-import Box from "@mui/material/Box";
+import Link from "next/link";
+
+function Button(props: { href: string, label: string }) {
+  return (
+    <Link href={props.href} className="flex items-center gap-5 rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base self-center">
+      <span>{props.label}</span>
+    </Link>
+  )
+}
+
+function DummyButton(props: { href: string, label: string }) {
+  return (
+    <div className="self-center">
+      {props.label}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <Container>
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Link href="/game"><Button variant="contained">Host Game</Button></Link>
-        <Button variant="contained">Join Game</Button>
-      </Box>
-    </Container>
+    <main className="flex min-h-screen">
+      <div className="flex flex-col gap-2 self-center flex-grow">
+        <Button href="/game" label="Host Game" />
+        <Button href="/join-game" label="Join Game" />
+      </div>
+    </main>
   );
 }
